9e47adc614980e0000626ae40c7407c3e26a37fa
Erdoğan FIRAT <efirat3@gmail.com>
2024-03-24 18:50:47 +0300

    useref2 ornegi yapildi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useEffect, useRef, useState } from "react";
+
 function App() {
+  const [sayi, setSayi] = useState(0);
+  const [can, setCan] = useState(3);
+
+  const sayiRef = useRef(0);
+
+  useEffect(()=>{
+    sayiRef.current++
+
+    console.log("Sayı değişti : ", sayi);
+    console.log("SayıRef değişti : ", sayiRef.current -1);
+
+  }, [sayi]);
+
   return (
     <div>
-      App
+      <p>Sayı : {sayi} | Can : {can}</p>
+      <button onClick={()=>setSayi(eskiDeger=>eskiDeger+5)}>Sayıyı Arttır (+5)</button>
+      <button onClick={()=>setSayi(eskiDeger=>eskiDeger-3)}>Sayıyı Azalt (-3)</button>
+
+      <button onClick={()=>setCan(eskiDeger=>eskiDeger-1)}>Canı Azalt (-1)</button>
     </div>
   );
 }
