823e2c9c566c9f93ff1280f8d81b7d9a86d0da96
Erdoğan FIRAT <efirat3@gmail.com>
2024-03-24 19:01:00 +0300

    useRef3 ornegi yapildi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-function App() {
+import React, { useEffect, useRef } from "react";
+
+const App = () => {
+  const inputRef = useRef();
+  const goster = true
+
+  useEffect(()=>{
+    //document.querySelector("#elem1").focus()
+
+  }, [])
+
+  const onButtonClick = () => {
+    console.log(inputRef.current?.focus());
+  };
+
   return (
     <div>
-      App
+      {goster && <input id="elem1" ref={inputRef} />}
+      <button onClick={onButtonClick}>Butona Tıkla</button>
     </div>
   );
-}
+};
 
 export default App;
