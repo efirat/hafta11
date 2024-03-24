00f0bf2c818bf6908df836ce1c375ee4f30315b4
Erdoğan FIRAT <efirat3@gmail.com>
2024-03-24 19:24:34 +0300

    useref4 ornegi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-function App() {
+import React, { useEffect, useRef, useState } from "react";
+
+const App = ({ prop }) => {
+
+  const [sayac, sayacGuncelle] = useState(0) // ⬅ bu kod sadece ilk renderda çalışır
+  const prevPropRef = useRef(prop) // ⬅ bu kod sadece ilk renderda çalışır
+
+  useEffect(() => {
+    console.log("Önceki prop:", prevPropRef.current);
+    console.log("Güncel prop:", prop);
+    
+    prevPropRef.current = prop
+  }, [prop]);
+
   return (
     <div>
-      App
+      <p>Prop: {prop}</p>
     </div>
   );
-}
+};
 
 export default App;
